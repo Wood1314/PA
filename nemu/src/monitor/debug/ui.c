ee826e902f2218624c4129ffb4a208da74a3a041
tracer-ics2017 <tracer@njuics.org>
2020-02-23 20:24:34 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:19:15 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:24:34 up 11:54,  1 user,  load average: 0.00, 0.00, 0.00 9c6ecd6cde17f858281447aa3b530dd7c7ae9727

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,12 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args){
+  char *n_str = strtok(args," ");
+  printf("%s", n_str);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Execute the step by one", cmd_si}
 
   /* TODO: Add more commands */
 
