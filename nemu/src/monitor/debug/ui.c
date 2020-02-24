14f3c97a8ce55924d7c056710369b7ab39109627
tracer-ics2017 <tracer@njuics.org>
2020-02-24 09:20:32 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:19:15 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:20:32 up 14:53,  1 user,  load average: 0.08, 0.07, 0.02 d65862c3260852d4e9a1cc1a5bf4e47bbbbb1e0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -2,7 +2,6 @@
 #include "monitor/expr.h"
 #include "monitor/watchpoint.h"
 #include "nemu.h"
-
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -74,11 +73,12 @@ static int cmd_info(char *args){
 static int cmd_x(char *args){
     if(args == NULL){printf("Please input argument\n"); return 0;}
     else{
-        printf("%-10s%-10s","Address","DwordBlock");
+        printf("%-10s%-10s%-10s\n","Address","DwordBlock","DwordBlock");
         char *n_str = strtok(args, " ");
-        printf("%s\n",n_str);
-        n_str = strtok(NULL," ");
-        printf("%s\n",n_str);
+        if(!memcpy(n_str,"0x",2)){
+           long addr = strtol(n_str,NULL,16);
+           printf("%#x",vaddr_read(addr,4)); 
+        }       
     }
     return 0;
 }
