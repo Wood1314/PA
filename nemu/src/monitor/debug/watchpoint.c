e7dc0533348606cf42633283b5938dfe9276608d
tracer-ics2017 <tracer@njuics.org>
2020-03-29 14:04:26 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:19:15 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:04:26 up 1 day, 17:31,  1 user,  load average: 0.25, 0.09, 0.02 f47547be51c7e8f5a4b9cfb78bb211a9b2b6bd9

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -168,7 +168,7 @@ void scan_breakpoint(){
     while(head2){
         if(head2->type == BREAK_POINT){
             vaddr_write(head2->breakpoint_addr,head2->op,1);
-           // printf("%#x\n",head2->op);
+            printf("%#x\n",head2->op);
         }
         head2 = head2->next;
     }
