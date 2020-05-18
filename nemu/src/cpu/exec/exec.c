e8a5130c254c4413bce7861b16234f31cf0e0b97
tracer-ics2017 <tracer@njuics.org>
2020-05-18 16:40:18 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:03:31 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:40:18 up 15:00,  1 user,  load average: 0.15, 0.12, 0.09 3d258d2dd693254a9e79af2e188128399a65b6ee

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -91,7 +91,7 @@ opcode_entry opcode_table [512] = {
   /* 0x40 */	IDEX(r, inc), EMPTY, EMPTY, EMPTY,
   /* 0x44 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x48 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x4c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x4c */	EMPTY, EMPTY, EMPTY, IDEX(r, dec),
   /* 0x50 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),
   /* 0x54 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),
   /* 0x58 */	IDEX(r,pop), IDEX(r,pop), IDEX(r,pop), IDEX(r,pop),
