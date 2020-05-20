1a5567f04662a1b244829bd81807bfc720cdd1af
tracer-ics2017 <tracer@njuics.org>
2020-05-20 20:35:10 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:03:31 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:35:10 up 1 day,  7:31,  1 user,  load average: 0.33, 0.28, 0.22 244fdb4e5ecc1257aefe9f361d8983fe81898240

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -51,14 +51,14 @@ make_EHelper(cwtl) {
     rtl_msb(&t1, &t0, 2);
     t3 = t1 == 0 ? 0 : ~0;
     rtl_mv(&t2, &t3);
-    // rtl_sr(R_DX, &t2, 2);
+    rtl_sr(R_DX,2, &t2);
   }
   else {
     rtl_lr(&t0, R_EAX, 4);
     rtl_msb(&t1, &t0, 4);
     t3 = t1 == 0 ? 0 : ~0;
     rtl_mv(&t2, &t3);
-    // rtl_sr(R_EDX, &t2, 4); 
+    rtl_sr(R_EDX, 4, &t2); 
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
