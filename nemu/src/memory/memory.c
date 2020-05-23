90290254e643083a8243da8d7be2d189747a9525
tracer-ics2017 <tracer@njuics.org>
2020-05-23 23:09:31 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:03:31 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:09:31 up 2 days,  5:02,  1 user,  load average: 1.08, 1.10, 1.09 88d94a973586a52374f3257ee4aa77d33bb30b3f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,6 @@
 #include "nemu.h"
+#include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +14,26 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int mapNo = is_mmio(addr);
+  if(mapNo != -1){
+      return mmio_read(addr, len, mapNo);
+  }
+  else
+  {
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
+  
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int mapNo = is_mmio(addr);
+  if(mapNo != -1){
+    mmio_write(addr, len, data, mapNo);
+  }
+  else
+  {
+    memcpy(guest_to_host(addr), &data, len);
+  }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
