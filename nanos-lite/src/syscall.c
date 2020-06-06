69c6440b4a0b8f18ea527c1c647350bb71373da9
tracer-ics2017 <tracer@njuics.org>
2020-06-06 21:19:16 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:19:16 up  1:11,  1 user,  load average: 0.39, 0.10, 0.14 448c47249651ea29d91bfaaf918606e1b0691bcf

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,6 +16,7 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
+  Log("1111");
   return fs_write(fd, (void*)buf, len);
 }
 
