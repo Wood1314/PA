656a3b62fe5b437e732fc994d204afe8872eeca0
tracer-ics2017 <tracer@njuics.org>
2020-06-07 17:23:49 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:23:49 up  2:10,  1 user,  load average: 0.34, 0.20, 0.17 636b679e332d3ec224f177223f64a9068b625b5

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -17,41 +17,16 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  if (decoding.is_operand_size_16){
-    rtl_lr(&t1, R_SP, 2);
-    rtl_lr(&t0, R_AX,2);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_CX, 2);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_DX, 2);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_BX, 2);
-    rtl_push(&t0);
-    rtl_push(&t1);
-    rtl_lr(&t0, R_BP, 2);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_SI, 2);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_DI, 2);
-    rtl_push(&t0);
-  }else {
-    rtl_lr(&t1, R_ESP, 4);
-    rtl_lr(&t0, R_EAX, 4);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_ECX, 4);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_EDX, 4);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_EBX, 4);
-    rtl_push(&t0);
-    rtl_push(&t1);
-    rtl_lr(&t0, R_EBP, 4);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_ESI, 4);
-    rtl_push(&t0);
-    rtl_lr(&t0, R_EDI, 4);
-    rtl_push(&t0);
-  }
+  // TODO();
+  t0 = cpu.esp;
+  rtl_push(&cpu.eax);
+  rtl_push(&cpu.ecx);
+  rtl_push(&cpu.edx);
+  rtl_push(&cpu.ebx);
+  rtl_push(&t0);
+  rtl_push(&cpu.ebp);
+  rtl_push(&cpu.esi);
+  rtl_push(&cpu.edi);
 
   print_asm("pusha");
 }
