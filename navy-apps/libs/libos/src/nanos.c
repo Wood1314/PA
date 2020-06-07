c0c0099fa573102abf649079a451ae1eb4d402a0
tracer-ics2017 <tracer@njuics.org>
2020-06-07 11:21:54 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:21:54 up  1:29,  1 user,  load average: 0.18, 0.13, 0.11 68d54db9e623fec465ecf6376492af71b0772ef

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -8,7 +8,8 @@
 
 // TODO: discuss with syscall interface
 #ifndef __ISA_NATIVE__
-extern char end;
+extern char _end;
+intptr_t program_break = (intptr_t)&_end;
 // FIXME: this is temporary
 
 int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
@@ -29,26 +30,36 @@ int _write(int fd, void *buf, size_t count){
   return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
+// void *_sbrk(intptr_t increment){
+//   static int _end = 0;
+//   if( _end == 0) {
+//     _end = &end;
+//   }
+//   char *new_end = _end + increment;
+//   char tmp[100];
+//   sprintf(tmp, "brk:%p, incre:%p, new_end:%p, new_new_end:%p\n", _end, increment, new_end, (uintptr_t)new_end);
+//   _write(1, tmp, 100);
+//   int ret = _syscall_(SYS_brk,(uintptr_t)new_end, 0, 0);
+//   if(ret == 0){
+//     char temp2[100];
+//     void *old_end = _end;
+//     _end = new_end;
+//     sprintf(temp2, "Success! and update end:%p\n", _end);
+//     _write(1, temp2, 100);
+//     return (void *)old_end;
+//   }        
+//   else{
+//     return (void*)-1;
+//   }
+// }
 void *_sbrk(intptr_t increment){
-  static int _end = 0;
-  if( _end == 0) {
-    _end = &end;
+  intptr_t pre_pb = program_break;
+  if (_syscall_(SYS_brk, pre_pb, 0, 0) == 0) {
+    program_break += increment; 
+    return (void *)pre_pb;
   }
-  char *new_end = _end + increment;
-  char tmp[100];
-  sprintf(tmp, "brk:%p, incre:%p, new_end:%p, new_new_end:%p\n", _end, increment, new_end, (uintptr_t)new_end);
-  _write(1, tmp, 100);
-  int ret = _syscall_(SYS_brk,(uintptr_t)new_end, 0, 0);
-  if(ret == 0){
-    char temp2[100];
-    void *old_end = _end;
-    _end = new_end;
-    sprintf(temp2, "Success! and update end:%p\n", _end);
-    _write(1, temp2, 100);
-    return (void *)old_end;
-  }        
-  else{
-    return (void*)-1;
+  else {
+    return (void *)-1;
   }
 }
 
