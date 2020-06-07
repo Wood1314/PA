ef9cec1ed4d3863f13e4df2ae17e5fa0251a1a55
tracer-ics2017 <tracer@njuics.org>
2020-06-07 10:52:23 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:52:23 up  1:00,  1 user,  load average: 0.25, 0.26, 0.30 386014f4fae79cec3a0a4f81743c3873290e97c8

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -35,9 +35,9 @@ void *_sbrk(intptr_t increment){
     _end = &end;
   }
   char *new_end = _end + increment;
-  char tmp[100];
-  sprintf(tmp, "brk:%p, incre:%p\n", new_end, increment);
-  _write(1, tmp, 100);
+  // char tmp[100];
+  // sprintf(tmp, "brk:%p, incre:%p\n", new_end, increment);
+  // _write(1, tmp, 40);
   int ret = _syscall_(SYS_brk,(uintptr_t)new_end, 0, 0);
   if(ret == 0){
     void *old_end = _end;
