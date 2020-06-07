fc45a1e0b04087d3a515dccb451d5850191245c5
tracer-ics2017 <tracer@njuics.org>
2020-06-07 17:03:35 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:03:35 up  1:50,  1 user,  load average: 0.20, 0.12, 0.18 d5444b8fcb8d78ca7e26f4073d3eb25f48ab4fe2

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -15,51 +15,30 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-// int mm_brk(uint32_t new_brk) {
-//   Log("new_brk: %d\n", new_brk);
-//   if (current->cur_brk == 0) {
-//     current->cur_brk = current->max_brk = new_brk;
-//   }
-//   else {
-//     if (new_brk > current->max_brk) {
-//       Log("current is %d\n", current->max_brk);
-//       uintptr_t page_start = PGROUNDUP(current->max_brk);
-//       Log("page_start: %d\n", page_start + PGSIZE);
-//       uintptr_t page_end = PGROUNDUP(new_brk);
-//       for (; page_start <= page_end; page_start += PGSIZE) {
-//         _map(&current->as, (void *)page_start, new_page());
-//       }
-//       Log("Success!\n");
-//       current->max_brk = new_brk;
-//     }
-//     current->cur_brk = new_brk;
-//   }
-//   // success
-//   return 0;
-// }
 int mm_brk(uint32_t new_brk) {
+  Log("new_brk: %d\n", new_brk);
   if (current->cur_brk == 0) {
     current->cur_brk = current->max_brk = new_brk;
   }
   else {
     if (new_brk > current->max_brk) {
-      // TODO: map memory region [current->max_brk, new_brk)
-      // into address space current->as
-      if (new_brk & (PGSIZE - 1))
-        new_brk = (new_brk & ~(PGSIZE - 1)) + PGSIZE;
-      if (current->cur_brk & (PGSIZE - 1))
-        current->cur_brk = (current->cur_brk & ~(PGSIZE - 1)) + PGSIZE;
-      Log("cur->brk %d, new->brk %d", current->cur_brk, new_brk);
-      for (; current->cur_brk < new_brk; current->cur_brk += PGSIZE){
-        _map(&current->as, (void *)current->cur_brk, new_page());
+      Log("current is %d\n", current->max_brk);
+      uintptr_t page_start = PGROUNDUP(current->max_brk);
+      Log("page_start: %d\n", page_start + PGSIZE);
+      uintptr_t page_end = PGROUNDUP(new_brk);
+      for (; page_start <= page_end; page_start += PGSIZE) {
+        Log("new_page");
+        _map(&current->as, (void *)page_start, new_page());
       }
+      Log("Success!\n");
       current->max_brk = new_brk;
     }
-
     current->cur_brk = new_brk;
   }
+  // success
   return 0;
 }
+
 
 void init_mm() {
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
