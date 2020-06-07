54503b3e8d4978616c92b211ee004c5e5297d09d
tracer-ics2017 <tracer@njuics.org>
2020-06-07 17:30:18 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:30:18 up  2:17,  1 user,  load average: 0.07, 0.12, 0.14 675c1c6b16397fab4e1e5d04dfa8fd5ebb947d4

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -20,7 +20,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_BE: *dest = cpu.CF | cpu.ZF ? 1 : 0; break;
     case CC_S:  *dest = cpu.SF ? 1 : 0; break;
     case CC_L:  *dest = cpu.SF != cpu.OF ? 1 : 0; break;
-    case CC_LE: *dest = cpu.SF != cpu.OF || cpu.ZF ? 1 : 0; break;
+    case CC_LE: *dest = (cpu.SF != cpu.OF) || (cpu.ZF) ? 1 : 0; break;
 
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
