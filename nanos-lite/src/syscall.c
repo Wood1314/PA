7884ea590979d66a9a604a022fb96cf293834649
tracer-ics2017 <tracer@njuics.org>
2020-06-07 11:41:27 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:41:27 up  1:49,  1 user,  load average: 0.17, 0.15, 0.11 19276df84216ea0d7c95b6947610862173d9c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -45,7 +45,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
 
   uintptr_t ret = -1;
-  printf("a[0]:%p, a[1]:%p, a[2]:%p, a[3]:%p\n", a[0], a[1], a[2], a[3]);
+  printf("\n a[0]:%p, a[1]:%p, a[2]:%p, a[3]:%p\n", a[0], a[1], a[2], a[3]);
   switch (a[0]) {
     case SYS_none: ret = 1; break;
     case SYS_write: ret = sys_write(a[1], a[2], a[3]); break;
