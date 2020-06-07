5220b7ecdcf8ebed295205990b17751ccfb4480f
tracer-ics2017 <tracer@njuics.org>
2020-06-07 09:30:14 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:30:14 up  2:18,  1 user,  load average: 0.25, 0.23, 0.18 db6c23ff38300a432eefa9a57fc5386fd7e28af7

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -27,6 +27,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   assert(12);
+  Log('aaaaaa');
   return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
