92705fa4567983ad419106b1cc96b74ca9bd6809
tracer-ics2017 <tracer@njuics.org>
2020-06-08 19:19:03 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:19:03 up  4:17,  1 user,  load average: 0.24, 0.16, 0.17 f2008701289b2605123607c7948d7b3d30f15ebd

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -8,8 +8,7 @@
 
 // TODO: discuss with syscall interface
 #ifndef __ISA_NATIVE__
-extern char end;
-intptr_t program_break = (intptr_t)&end;
+
 // FIXME: this is temporary
 
 int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
@@ -23,12 +22,14 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
+  return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
   return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
+extern char end;
+intptr_t program_break = (intptr_t)&end;
 
 void *_sbrk(intptr_t increment){
   intptr_t pre_pb = program_break;
@@ -42,15 +43,15 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
+  return _syscall_(SYS_read, fd, (uintptr_t)buf, count); 
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
+  return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 // The code below is not used by Nanos-lite.
