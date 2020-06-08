73b14975adf1cb75164304ec11980b034bc2a00b
tracer-ics2017 <tracer@njuics.org>
2020-06-08 18:05:56 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:05:56 up  3:05,  1 user,  load average: 0.16, 0.09, 0.02 22807f5c1a22e4e466b4924c8079d240174b0e83

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,9 +11,11 @@ extern int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
-  int fd = fs_open("/bin/text", 0, 0);
-  Log("filename is %s",filename);
+  int fd = fs_open(filename, 0, 0);
+  Log("filename is ");
   fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); 
+  Log("filename is ");
   fs_close(fd); 
+  Log("filename is ");
   return (uintptr_t)DEFAULT_ENTRY;
 }
