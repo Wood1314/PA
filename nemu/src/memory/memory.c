089a97e63ef55bf35d7464fdacd30ae9631e009b
tracer-ics2017 <tracer@njuics.org>
2020-06-17 21:03:28 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:40:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:03:28 up 21:25,  1 user,  load average: 0.30, 0.25, 0.13 da6372d76e483b28c3838c795326dc9e64d5ab8d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,6 +10,7 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
+paddr_t page_translate(vaddr_t vaddr, bool is_write);
 
 /* Memory accessing interfaces */
 
@@ -37,9 +38,71 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(cpu.cr0.paging) {
+      bool overflow_page = (addr >> 12) != ( (addr  + len -1) >> 12 );
+      if (overflow_page) {
+        assert(0);
+      }
+      else {
+         paddr_t paddr = page_translate(addr, false);
+         return paddr_read(paddr, len);
+      }
+  }
+  else
+    return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if(cpu.cr0.paging) {
+      bool overflow_page = (addr >> 12) != ( (addr + len -1) >> 12 );
+      if (overflow_page) {
+        assert(0);
+      }
+      else {
+          paddr_t paddr = page_translate(addr, true);
+          paddr_write(paddr, len, data);
+          return;
+      }
+  }
+  else
+    paddr_write(addr, len, data);
+    return;
+}
+
+paddr_t page_translate(vaddr_t vaddr, bool is_write) {
+    if(cpu.cr0.paging == 0)
+      return vaddr;
+    uint32_t pde_index = (vaddr >> 22) & 0x3ff;
+    uint32_t pte_index = (vaddr >> 12) & 0x3ff;
+    uint32_t off = vaddr &0xfff;
+
+    //find pde
+    uint32_t pde_base = cpu.cr3.page_directory_base;
+    uint32_t pde = (pde_base << 12) + (pde_index << 2);
+    PDE pde_obj;
+    pde_obj.val = paddr_read(pde, 4);
+    Log("ped is 0x%x", pde_obj.val);
+    assert(pde_obj.val);
+
+    //find pte
+    uint32_t pte = (pde_obj.val & 0xfffff000 ) + (pte_index << 2);
+    PTE pte_obj;
+    pte_obj.val = paddr_read(pte, 4);
+    Log("pte is 0x%x", pte_obj.val);
+    assert(pte_obj.val);
+
+
+    if (!pde_obj.accessed) {
+        pde_obj.accessed = 1;
+        paddr_write(pde, 4, pde_obj.val);
+    }
+
+    if (!pte_obj.accessed || ( pte_obj.dirty == 0 && is_write)) {
+        pte_obj.accessed = 1;
+        paddr_write(pte, 4, pte_obj.val);
+    }
+
+    uint32_t paddr = (pte_obj.val & 0xfffff000) | off;
+    Log("paddr is 0x%x", paddr);
+    return paddr;
 }
