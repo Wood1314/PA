68461d42eafefa76dd98de9d13bfb879f360a4ec
tracer-ics2017 <tracer@njuics.org>
2020-06-26 21:07:56 +0800

    >  compile 161820323 wood Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:40:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:07:56 up 18:19,  1 user,  load average: 0.46, 0.18, 0.16 85f90e216e37f013778177d7cce182f613c1e37d

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -16,7 +16,7 @@ _RegSet* irq_handle(_RegSet *tf) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_TRAP; break;
       case 32: ev.event = _EVENT_IRQ_TIME; break;
-      default: ev.event = _EVENT_TRAP; break;
+      default: ev.event = _EVENT_ERROR; break;
     }
 
     next = H(ev, tf);
