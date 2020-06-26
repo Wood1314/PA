b2919a6307e487e38987f71cee60aff013bc5d57
tracer-ics2017 <tracer@njuics.org>
2020-06-26 20:41:24 +0800

    >  run 161820323 wood Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:40:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:41:24 up 17:53,  1 user,  load average: 0.61, 0.37, 0.20 8f62775e136142d92fa72b1bdc327ed7778b695e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,6 +6,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
       case _EVENT_SYSCALL: return do_syscall(r); break;
       case _EVENT_TRAP:  return schedule(r); break;
+      case 3: Log("aaa"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return NULL;
